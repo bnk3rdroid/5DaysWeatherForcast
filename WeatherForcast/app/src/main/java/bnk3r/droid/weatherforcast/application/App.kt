91b5513e70fde410a1f8c1178725cf51229374ba
bnk3r <bnk3r.droid@gmail.com>
2016-11-01 11:44:26 +0100

    Application class + kotlin add in gradle

---
+++ WeatherForcast/app/src/main/java/bnk3r/droid/weatherforcast/application/App.kt
@@ -0,0 +1,11 @@
+package bnk3r.droid.weatherforcast.application
+
+import android.app.Application
+
+/**
+ * Application class
+ * Created by YomanHD on 01/11/2016.
+ */
+class App: Application() {
+
+}
